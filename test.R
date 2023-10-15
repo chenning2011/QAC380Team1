bb3260854b62b87ce57f16e414721583ee33731f
chenning2011 <chenning@wesleyan.edu>
2023-10-15 16:24:27 -0400

    trtubg

--- test.R
+++ test.R
@@ -1,1 +1,12 @@
-#new r script, seeing if this works at all 
+#team 1, loading in dataset (everyone will probably need diff. load lines, unless someone can get the excel data to work in github)
+
+library(readxl)
+#caleb load line
+HHSNeedsAssessment_FINAL_DATA <- read_excel("~/Desktop/Folder/University/Junior Year/F23/QAC380/HHSNeedsAssessment FINAL DATA.xlsx")
+
+
+require(ggplot2)
+require(dplyr)
+require(tidyr)
+require(descr)
+require(gert)
