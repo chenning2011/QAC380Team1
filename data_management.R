7dd6949c9d6c3e0fbe258ff75e2cb3fa5fceb34a
chenning2011 <chenning@wesleyan.edu>
2023-10-15 19:06:43 -0400

    updating

--- data_management.R
+++ data_management.R
@@ -4,18 +4,21 @@ library(readxl)
 #caleb load line
 HHSNeedsAssessment_FINAL_DATA <- read_excel("~/Desktop/Folder/University/Junior Year/F23/QAC380/HHSNeedsAssessment FINAL DATA.xlsx")
 
-require(ggplot2)
-require(dplyr)
-require(tidyr)
-require(descr)
-require(gert)
+require(tidyverse)
 
-#working on subsetting the data
-myvars <- c("Record ID", "Age", "What is your current gender identity?", "Other gender identity",
+#taking a subset of the data
+myvars <- c("Record ID", "Survey Timestamp...3","Complete?...4" ,"Age", "What is your current gender identity?", "Other gender identity",
             "Which of these best describes your sexual orientation?", "Other sexual orientation",
-            "What is your race? (choice=White)", "What is your race (choice=Black or African American)",
+            "What is your race? (choice=White)", "What is your race? (choice=Black or African American)",
             "What is your race? (choice=American Indian or Alaska Native)", 
             "What is your race? (choice=Native Hawaiian or other Pacific Islander)",
-            "What is your race? (choice=Asian)", "What is your race? (choice=Other (Please specify):{other_race})",
-            "Other race", "What is the highest education level you have completed?",
+            "What is your race? (choice=Asian)",
+            "Other race","Are you Hispanic/Latinx?", "What is the highest education level you have completed?",
+            "Biggest health concern", "How could clinics help","Which clinic are currently visiting?"
             )
+HHS <-HHSNeedsAssessment_FINAL_DATA[myvars]
+
+#replacing NA values with Open Door Health for clinic name 
+HHS$`Which clinic are currently visiting?`[is.na(HHS$`Which clinic are currently visiting?`)] <- "Open Door Health"
+
+#next steps for data management here
